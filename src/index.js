ca230283dbc937799bd02447f21f044e2c5fe376
NMoore42 <nicholas.moore5@icloud.com>
2019-06-10 11:36:48 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink} from "react-router-dom";
+import Home from "./home"
+import About from "./about"
+import Login from "./login"
+import Signup from "./signup"
+import Messages from "./messages"
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
 
-ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
-);
+
+const Navbar = () =>
+  <div>
+    <NavLink to="/" exact style={link} activeStyle={{ background: 'darkblue' }}>Home</NavLink>
+    <NavLink to="/about" exact style={link} activeStyle={{ background: 'darkblue' }}>About</NavLink>
+    <NavLink to="/login" exact style={link} activeStyle={{ background: 'darkblue' }}>Login</NavLink>
+    <NavLink to="/signup" exact style={link} activeStyle={{ background: 'darkblue' }}>Signup</NavLink>
+    <NavLink to="/messages" exact style={link} activeStyle={{ background: 'darkblue' }}>Messages</NavLink>
+  </div>;
+
+ReactDOM.render((
+  <Router>
+    <React.Fragment>
+      <Navbar />
+      <Route exact path='/' component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="/messages" component={Messages} />
+    </React.Fragment>
+  </Router>
+),
+  document.getElementById("root")
+)
---
+++ src/messages.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+
+export default class Messages extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Message 1:
+          <p>This is a message</p>
+        </h1>
+        <h1>Message 2:
+          <p>This is a message</p>
+        </h1>
+      </div>
+    )
+  }
+}
